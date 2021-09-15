146ac423dadac6514f9511bc2cf7b82515d80f9a
Oliver Lantwin <oliver.lantwin@cern.ch>
2021-09-15 14:08:01 +0200

    Avoid duplication in plotting

--- plotting/event_display.py
+++ plotting/event_display.py
@@ -154,96 +154,14 @@ def plot_ed(nameout="", option=None, to_be_shown=False, adc_min=def_adc_min, adc
     run_day = str(dc.evt_list[-1].date)
     evt_nb = str(dc.evt_list[-1].evt_nb)
 
-    plt.savefig('ED/ed_'+nameout+option+'_'+run_day+'_evt_'+evt_nb+'.png')
+    plt.savefig(f'ED/ed_{nameout+option}_{run_day}_evt_{evt_nb}.png')
     if(to_be_shown):
         plt.show()
     plt.close()
 
 
-def plot_ed_data(option=None, to_be_shown=False, adc_min=def_adc_min, adc_max=def_adc_max):
-    plot_ed(nameout="data", option=option, to_be_shown=to_be_shown, adc_min=adc_min, adc_max=adc_max)
+def plot_ed_data(*args, **kwargs):
+    plot_ed(*args, nameout="data", **kwargs)
 
-
-
-
-def plot_ed_data_and_hits(option=None, to_be_shown=False, adc_min=def_adc_min, adc_max=def_adc_max):
-
-
-    fig = plt.figure(figsize=(9,4))
-    gs = gridspec.GridSpec(nrows=2, 
-                           ncols=2, 
-                           height_ratios=[1, 10])
-    ax_col = fig.add_subplot(gs[0,:])
-    ax_v0  = fig.add_subplot(gs[1, 0])
-    ax_v1  = fig.add_subplot(gs[1, 1], sharey=ax_v0)
-    
-
-    r0, r1 = [], []
-    for h in dc.hits_list:
-        if(h.view==0):
-            r0.append(patches.Rectangle((h.channel-0.5,h.start),1,h.stop-h.start,linewidth=1,edgecolor='k',facecolor='none'))
-        else:
-            r1.append(patches.Rectangle((h.channel-0.5,h.start),1,h.stop-h.start,linewidth=1,edgecolor='k',facecolor='none'))
-
-
-
-    """ draw data """
-
-    """ View 0 """
-    ax_v0 = draw(0, ax=ax_v0, adc_min=adc_min, adc_max=adc_max)
-    ylow, yup = ax_v0.get_ylim()
-    ax_v0.plot([32, 32], [ylow, yup], ls='--', c='k')
-    ax_v0.set_title('View 0 ' + '['+cf.view_type[0]+']')
-    ax_v0.set_ylabel('Time')
-    for r in r0:
-        ax_v0.add_patch(r)
-
-    """ View 1 """
-    ax_v1 = draw(1, ax=ax_v1, adc_min=adc_min, adc_max=adc_max)
-    ax_v1.set_title('View 1 ' + '['+cf.view_type[1]+']')
-    ax_v1.set_ylabel('Time')
-    ax_v1.yaxis.tick_right()
-    ax_v1.yaxis.set_label_position("right")
-    for r in r1:
-        ax_v1.add_patch(r)
-
-        
-    ax_v0.set_xlabel('View Channel')
-    ax_v1.set_xlabel('View Channel')
-
-
-
-    ax_v0.yaxis.set_major_locator(plt.MaxNLocator(4))
-
-    ax_col.set_title('Collected Charge [ADC]')
-        
-                         
-    cb = fig.colorbar(ax_v0.images[-1], cax=ax_col, orientation='horizontal')
-    cb.ax.xaxis.set_ticks_position('top')
-    cb.ax.xaxis.set_label_position('top')
-        
-    plt.tight_layout()
-    plt.subplots_adjust(wspace=0.02)
-
-    """
-    plt.subplots_adjust(wspace=0.02, 
-                        hspace=0.25, 
-                        top=0.92, 
-                        bottom=0.08, 
-                        left=0.1, 
-                        right=0.9)
-    """
-
-    if(option):
-        option = "_"+option
-    else:
-        option = ""
-
-    run_day = str(dc.evt_list[-1].date)
-    evt_nb = str(dc.evt_list[-1].evt_nb)
-
-    plt.savefig('ED/ed_data_and_hits'+option+'_'+run_day+'_evt_'+evt_nb+'.png')
-    if(to_be_shown):
-        plt.show()
-    plt.close()
-
+def plot_ed_data_and_hits(*args, **kwargs):
+    plot_ed(*args, draw_hits=True, nameout="data", **kwargs)
